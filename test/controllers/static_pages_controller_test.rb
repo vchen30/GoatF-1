b8f83889860f10c6d6f0d0f9a5fc416cf4ff9044
Andy <andy_tse28@hotmail.com>
2017-06-23 23:25:11 -0700

    more user models

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+  	get root_path
     assert_response :success
+    assert_select "title", "GoatF"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
+    #assert_select "title", "Help | GoatF"
   end
 
 end
