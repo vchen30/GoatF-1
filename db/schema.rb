a17f1cbf231c78fe886669cbe8c1928b7cd46488
Vic <vca30@sfu.ca>
2017-06-25 07:53:02 +0000

    fml

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170624232537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
