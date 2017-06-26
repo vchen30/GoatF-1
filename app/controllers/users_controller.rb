e7d50922ee8e6ec220c7ed8e0cf7b2e6c8420e15
Andy <andy_tse28@hotmail.com>
2017-06-26 07:21:14 -0700

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new 
   end
-  
+
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "You signed up successfully"
       flash[:color]= "valid"
@@ -14,4 +14,8 @@ class UsersController < ApplicationController
     end
     render "new"
   end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
